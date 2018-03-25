a642d004f8f68d9d718fb3bce77ad675bb3dd528
ERIC KELLY <eakelly220@gmail.com>
2018-03-25 04:59:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = {artist: "song"}
 
-function updatePlaylist (playlist, artist, song){
-   playlist.artist = [song]
-   return playlist
-}
+function updatePlaylist(playlist, artistName, songTitle){
+tryer = artistName;
+playlist.artist = [songTitle]
+return playlist
+  }
